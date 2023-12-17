0ede5b6525faa356339073d784440edbb0fcd8c2
Mike Ball <mikedball@gmail.com>
2023-12-17 10:36:03 -0500

    improve documentation of TF files

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,17 @@
 terraform {
   required_version = "1.5.0"
 
-  // Terraform workspaces assume a single S3 backend configuration spanning all
-  // workspaces, enabling re-use of a single Terraform project configuration
-  // across multiple workspace contexts.
-  // Terraform automatically saves each workspace's state to a distinct
-  // workspace object path:
-  // ${BUCKET}/env:/${terraform.workspace}/terraform.tfstate
+  # Terraform workspaces support a single S3 backend configuration spanning all
+  # workspaces, enabling re-use of a single Terraform project configuration
+  # across multiple workspace contexts.
+  #
+  # Terraform automatically saves each workspace's state to a distinct
+  # workspace object path:
+  # s3://${BUCKET}/env:/${terraform.workspace}/${KEY}
+  #
+  # If no workspace is specified, Terraform uses the 'default' workspace and saves
+  # the state to:
+  # s3://${BUCKET}/${KEY}
   backend "s3" {
     bucket                      = "tf-workspaces-demo"
     key                         = "terraform.tfstate"
