6789a1c64ce2b6f616a08bac71794d503ac1f4f5
Mike Ball <mikedball@gmail.com>
2023-12-17 10:01:49 -0500

    add documentation to locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,13 @@
+# Via Terraform locals, workspace-specific logic can be triggered without
+# leaking that logic beyond the Terraform project itself and complicating the
+# Terraform configuration's public interface via excessive variables whose
+# values must be determined via wrapper scripting and orchestration outside of
+# Terraform.
+#
+# As a convention, all the Terraform configuration's conditional logic may live
+# here, in a consolidated, easily-discoverable location within locals.tf.
+# Configuration elsewhere in the project may use these locals, but doesn't need
+# to repeat their logic in non-obvious dark corners of the project.
 locals {
   workspace_parts = split("_", terraform.workspace)
 
@@ -7,6 +17,11 @@ locals {
 
   env = local.workspace_parts[2]
 
+  # By creating a per-env allow list of accounts, the Terraform configuration
+  # itself can guard against instances where a workspace targets and undesired
+  # and problematic env/account combination, such as the use of a production
+  # account to create a dev environment:
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs#allowed_account_ids
   supported_accounts_per_env = {
     prod = ["123"]
     dev  = ["456"]
