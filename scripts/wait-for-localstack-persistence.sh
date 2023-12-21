cf128281bf45b648b5b9886f22c0ed54e1c816af
Mike Ball <mikedball@gmail.com>
2023-12-21 15:31:13 -0500

    attempt to improve persistance verification polling

--- scripts/wait-for-localstack-persistence.sh
+++ scripts/wait-for-localstack-persistence.sh
@@ -5,20 +5,22 @@
 # that is present in localstack-data/s3/assets is not yet reflected in
 # store.json, and is therefore unknown to the localstack S3 API.
 
-default_workspace_count="$(jq '.include | length' workspaces.json)"
-workspace_count="${1:-$default_workspace_count}"
+workspace="${1}"
 attempts=0
 max_attempts=5
 initial_wait_period="${2:-0}"
+query=".data[\"000000000000\"][\"us-east-1\"][\"_global\"][\"buckets\"][\"tf-workspaces-demo\"][\"objects\"][\"_store\"][\"env:/${workspace}/terraform.tfstate\"][\"key\"]"
 
 sleep "$initial_wait_period"
 
-until [ "$(jq '.data["000000000000"]["us-east-1"]["_global"]["buckets"]["tf-workspaces-demo"]["objects"]["_store"] | keys | length' localstack-data/s3/store.json)" -eq "${workspace_count}" ] ; do
-  if [ "${attempts}" -eq "${max_attempts}" ]; then
-    persisted_objects=$(jq '.data["000000000000"]["us-east-1"]["_global"]["buckets"]["tf-workspaces-demo"]["objects"]["_store"] | keys' localstack-data/s3/store.json)
+jq -r "${query}" localstack-data/s3/store.json
 
-    echo "Max attempts reached; expected ${workspace_count} workspace objects; found:"
-    echo "${persisted_objects}"
+until [ "$(jq -r "${query}" localstack-data/s3/store.json)" = "env:/${workspace}/terraform.tfstate" ]; do
+  if [ "${attempts}" -eq "${max_attempts}" ]; then
+    result=$(jq '.data["000000000000"]["us-east-1"]["_global"]["buckets"]["tf-workspaces-demo"]["objects"]["_store"]' localstack-data/s3/store.json)
+
+    echo "Max attempts reached; expected to find ${workspace} key in S3; found:"
+    echo "${result}"
     exit 1
   fi
 
