fac13d08ad923690bfbab4f4c7d930717a0a687a
Mike Ball <mikedball@gmail.com>
2023-12-21 09:14:19 -0500

    improve reliability of PR workspace destruction

--- scripts/wait-for-localstack-persistence.sh
+++ scripts/wait-for-localstack-persistence.sh
@@ -9,6 +9,9 @@ default_workspace_count="$(jq '.include | length' workspaces.json)"
 workspace_count="${1:-$default_workspace_count}"
 attempts=0
 max_attempts=5
+initial_wait_period="${2:-0}"
+
+sleep "$initial_wait_period"
 
 until [ "$(jq '.data["000000000000"]["us-east-1"]["_global"]["buckets"]["tf-workspaces-demo"]["objects"]["_store"] | keys | length' localstack-data/s3/store.json)" -eq "${workspace_count}" ] ; do
   if [ "${attempts}" -eq "${max_attempts}" ]; then
